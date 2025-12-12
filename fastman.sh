4912ae4a096588656e128e9e812178260f2ddc58
Alihan1ai9595 <112238568+Alihan1ai9595@users.noreply.github.com>
2025-12-12 20:20:18 +0500

    Update fastman.sh

--- fastman.sh
+++ fastman.sh
@@ -25,9 +25,8 @@ exists(){
 
 if exists pacman;then # Arch, Endeavour, Cachy, Manjaro etc.
 pacman -S reflector curl jq --needed --noconfirm
-mirror = /etc/pacman.d/mirrorlist
-reflector -c $(curl -s ipinfo.io/json | jq -r '.country') --save $mirror
-echo -e "\n\033[38;5;46mWe updated your pacman mirror file in $mirror.${RESET}"
+reflector -c $(curl -s ipinfo.io/json | jq -r '.country') --save /etc/pacman.d/mirrorlist
+echo -e "\n\033[38;5;46mWe updated your pacman mirror file in /etc/pacman.d/mirrorlist.${RESET}"
 
 else
 echo -e "${RED}You don't have pacman, it's obvious. Exiting.${RESET}"
